203a80296f4f1c296e1d21f0a5f2ff54efb1edf6
le.david <le.david@art1st.hu>
2022-12-05 15:50:50 +0100

    second commit

--- Example/TestTestTestPod/ViewController.swift
+++ Example/TestTestTestPod/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import TestTestTestPod
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let log = Logger()
+        log.Logthis()
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Logger.swift
@@ -0,0 +1,17 @@
+//
+//  Logger.swift
+//  TestTestTestPod
+//
+//  Created by David Sonlong Le on 2022. 12. 05..
+//
+
+import Foundation
+
+public class Logger {
+    public init() {
+        
+    }
+    public func Logthis() {
+        print("this")
+    }
+}
